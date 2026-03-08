75778443f819b4dfaa5b193cd8f4493eefb536e1
CyberPokemon <imoncoding@gmail.com>
2026-03-08 09:40:14 +0530

    file reading and saving to database under process

--- backend/src/main/java/com/glitchers/backend/service/FileStorageService.java
+++ backend/src/main/java/com/glitchers/backend/service/FileStorageService.java
@@ -1,14 +1,25 @@
 package com.glitchers.backend.service;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
+//import org.apache.commons.io;
 
 @Service
 public class FileStorageService {
@@ -16,15 +27,100 @@ public class FileStorageService {
     @Value("${upload.storage.directory}")
     public String STORAGE_DIRECTORY;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
 
     public void saveFile(MultipartFile fileToSave) throws IOException {
+
         if (fileToSave == null) {
             throw new NullPointerException("fileToSave is null");
         }
-        var targetFile = new File(STORAGE_DIRECTORY + File.separator + fileToSave.getOriginalFilename());
-        if (!Objects.equals(targetFile.getParent(), STORAGE_DIRECTORY)) {
-            throw new SecurityException("Unsupported filename!");
-        }
+
+        File targetFile = new File(STORAGE_DIRECTORY + File.separator + fileToSave.getOriginalFilename());
+
         Files.copy(fileToSave.getInputStream(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        // Process CSV
+        processCSV(targetFile);
+
+        // Delete original file
+        targetFile.delete();
+    }
+
+    private void processCSV(File csvFile) throws IOException {
+
+        Reader reader = new FileReader(csvFile);
+
+        CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader());
+
+        Map<String, Integer> headers = parser.getHeaderMap();
+
+//        String tableName = "dataset_" + UUID.randomUUID().toString().replace("-", "");
+
+        String fileName = csvFile.getName()
+                .replace(".csv", "")
+                .toLowerCase()
+                .replaceAll("[^a-z0-9]", "_");
+
+        String tableName = "dataset_" + fileName;
+
+        createTable(headers, tableName);
+
+        insertRows(parser, headers, tableName);
+    }
+
+
+    private void createTable(Map<String, Integer> headers, String tableName) {
+
+        StringBuilder sql = new StringBuilder("CREATE TABLE " + tableName + " (");
+
+//        for (String column : headers.keySet()) {
+//
+//            sql.append(column).append(" TEXT,");
+//        }
+
+        for (String column : headers.keySet()) {
+
+            String safeColumn = column
+                    .trim()
+                    .toLowerCase()
+                    .replaceAll("[^a-z0-9]", "_");
+
+            sql.append(safeColumn).append(" TEXT,");
+        }
+        sql.deleteCharAt(sql.length() - 1);
+
+        sql.append(")");
+
+        jdbcTemplate.execute(sql.toString());
+    }
+
+
+    private void insertRows(CSVParser parser,
+                            Map<String,Integer> headers,
+                            String tableName) {
+
+        for (CSVRecord record : parser) {
+
+            StringBuilder columns = new StringBuilder();
+            StringBuilder values = new StringBuilder();
+
+            for (String column : headers.keySet()) {
+
+                columns.append(column).append(",");
+                values.append("'")
+                        .append(record.get(column).replace("'", "''"))
+                        .append("'")
+                        .append(",");
+            }
+
+            columns.deleteCharAt(columns.length()-1);
+            values.deleteCharAt(values.length()-1);
+
+            String sql = "INSERT INTO " + tableName + " (" + columns + ") VALUES (" + values + ")";
+
+            jdbcTemplate.execute(sql);
+        }
     }
 }
