e40565f498aee67dcf5df08b794683881c6cad1c
CyberPokemon <imoncoding@gmail.com>
2026-03-08 19:38:58 +0530

    api added

--- backend2/app.py
+++ backend2/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from main import generate_dashboard_logic
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="AI Dashboard API",
@@ -8,16 +9,26 @@ app = FastAPI(
     version="1.0"
 )
 
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins (good for development)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class QueryRequest(BaseModel):
     question: str
+
 
 @app.get("/")
 def home():
     return {"message": "AI Dashboard API is running"}
 
+
 @app.post("/generate-dashboard")
 def generate_dashboard(request: QueryRequest):
-    
     try:
         result = generate_dashboard_logic(request.question)
         return {
