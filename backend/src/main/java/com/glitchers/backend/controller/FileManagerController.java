fc6b36b186bd91e820f49320a6954f3321faf675
CyberPokemon <imoncoding@gmail.com>
2026-03-08 08:01:19 +0530

    file uploading api created

---
+++ backend/src/main/java/com/glitchers/backend/controller/FileManagerController.java
@@ -0,0 +1,32 @@
+package com.glitchers.backend.controller;
+
+import com.glitchers.backend.dto.ApiResponseDTO;
+import com.glitchers.backend.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/filesapi")
+public class FileManagerController {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @PostMapping("/upload-file")
+    public ResponseEntity<ApiResponseDTO> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            fileStorageService.saveFile(file);
+            return ResponseEntity.ok(new ApiResponseDTO("File Uploaded"));
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().body(new ApiResponseDTO("file not uploaded"));
+        }
+    }
+
+}
---
+++ backend/src/main/java/com/glitchers/backend/service/FileStorageService.java
@@ -0,0 +1,30 @@
+package com.glitchers.backend.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+@Service
+public class FileStorageService {
+
+    @Value("${upload.storage.directory}")
+    public String STORAGE_DIRECTORY;
+
+
+    public void saveFile(MultipartFile fileToSave) throws IOException {
+        if (fileToSave == null) {
+            throw new NullPointerException("fileToSave is null");
+        }
+        var targetFile = new File(STORAGE_DIRECTORY + File.separator + fileToSave.getOriginalFilename());
+        if (!Objects.equals(targetFile.getParent(), STORAGE_DIRECTORY)) {
+            throw new SecurityException("Unsupported filename!");
+        }
+        Files.copy(fileToSave.getInputStream(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+}
